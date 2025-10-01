f800a806ed1ce946cb06fea8a7a024d48d501115
Eduarda Groehs <eduardagroehs@gmail.com>
2025-10-01 19:32:33 -0300

    Adicionar arquivos de projeto.

---
+++ desafio-adonet/Banco/Connection.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace desafio_adonet.Banco
+{
+    internal class Connection
+    {
+        private string connectionString = "Data Source=DBS-NOTE-889\\SQLEXPRESS11;Initial Catalog=DesafioAdoNet;Integrated Security=True;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False";
+
+        public SqlConnection ObterConexao()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
+
+//try
+//{
+//    using var connection = new Connection().ObterConexao();
+//   connection.Open();
+//} catch (Exception ex)
+//{
+//   Console.WriteLine(ex.Message);
+//}
